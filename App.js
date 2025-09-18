0b88442f115d2175fb098a3877d0337d68476005
Chinhle0803 <thongchinhle.12c2@gmail.com>
2025-09-19 00:51:03 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen01 from "./screen/Screen01"
+import Screen02 from "./screen/Screen02"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Screen01/>
+    <Screen02/>
   );
 }
 
